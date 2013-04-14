da38bdf96f06d012c99f4446b7a288c4a02383d0
Michael Scott Hertzberg <mshertzberg@gmail.com>
2013-04-13 18:55:18 -0600

    edits to key vals; remove functions-*

--- local-config.php
+++ local-config.php
@@ -1,12 +1,9 @@
 <?php
-/*
-This is a sample local-config.php file
-In it, you *must* include the four main database defines
+/**
+ * This file only exists for local development environment, which supersedes wp-config.php if exists
+ */
 
-You may include other settings here that you only want enabled on your local development checkouts
-*/
-
-define( 'DB_NAME', 'mimediacms' );
-define( 'DB_USER', 'root' );
+define( 'DB_NAME', '' );
+define( 'DB_USER', '' );
 define( 'DB_PASSWORD', '' );
-define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+define( 'DB_HOST', 'localhost' );
